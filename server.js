ee8506dda156f7da037bd348f4b986c80a6f8f04
Krishnan <52234160+krish-45@users.noreply.github.com>
2024-05-27 23:24:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const port = 3030;
 
 // Define a simple API endpoint
 app.get('/api/v1/hello', (req, res) => {
-  res.json({ message: `Hi, This is Server 1: ${port}!` });
+  res.status(200).json({ message: `Hi, This is Server 1: ${port}!` });
 });
 
 app.get('/health-check', (req, res) => {
-  res.json({ message: `Health Check 1: ${port}!` });
+  res.status(200).json({ message: `Health Check 1: ${port}!` });
 });
 
 app.get('*', (req, res) => {
-  res.json({ message: `* Executed: ${port}!` });
+  res.status(200).json({ message: `* Executed: ${port}!` });
 });
 
 // Start the server
